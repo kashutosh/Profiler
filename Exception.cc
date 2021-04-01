ea7218b3bf36ba48bb0ff590a90a9cdf785506dc
Panther <pantherhelfie@gmail.com>
2021-04-01 22:03:52 +0530

    Creating base class for Exception Handling
    	new file:   Exception.cc
    	new file:   Exception.h

---
+++ Exception.cc
@@ -0,0 +1,41 @@
+#include "Exception.h"
+#include <iostream>
+#include <typeinfo>
+using namespace std;
+
+class Polymorphic {virtual void member(){}};
+void foo() {
+
+  try
+  {
+    Polymorphic * pb = 0;
+    typeid(*pb);  // throws a bad_typeid exception
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << "exception caught: " << e.what() << '\n';
+    throw;
+  }
+
+    string s("Hit an exception in foo");
+    Exception e(s.c_str());
+    throw e;
+}
+
+int main() {
+    string s("Hit an exception");
+    Exception e(s.c_str());
+    cout << e.what() << endl;
+
+    try {
+        foo();
+    }
+    catch (Exception &e) {
+        cout << e.what() << endl;
+    }
+    catch (exception &e) {
+
+        cout << "Exception caught in main " << e.what() << endl;
+    }
+    return 0;
+}
---
+++ Exception.h
@@ -0,0 +1,25 @@
+#ifndef EXCEPTION_H
+#define EXCEPTION_H
+
+#include <exception>
+#include <string>
+using namespace std;
+
+
+class Exception : public exception{
+private:
+    string msg_;
+public:
+    Exception (Exception &e) {
+    }
+    Exception(const char *message) : msg_(message){
+    }
+
+    void setMessage(string message) {
+    }
+    virtual const char* what() const throw() {
+        return msg_.c_str();
+    }
+};
+
+#endif
