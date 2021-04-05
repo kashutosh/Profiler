341606943994da5c5e4d6400d32a665c8122ee81
Panther <pantherhelfie@gmail.com>
2021-04-05 11:45:02 +0530

    Added a Key-Value store of <string, string>
    to facilitate creation of flight recording objects
    
     Changes to be committed:
    	new file:   KeyValueStore.cc
    	new file:   KeyValueStore.h
    	modified:   CMakeLists.txt

---
+++ KeyValueStore.cc
@@ -0,0 +1,48 @@
+#include "KeyValueStore.h"
+
+KeyValueStore *KeyValueStore ::keyvalue_store = nullptr;
+
+
+bool KeyValueStore::print() {
+    return true;
+}
+
+int KeyValueStore::getObjectsCount() const{
+    return this->keyValuePairs.size();
+}
+
+bool KeyValueStore::emptyKeyValueStore() {
+    return true;
+}
+
+bool KeyValueStore::checkIfThisKeyExists(const string key) {
+    if (keyValuePairs.find(key) == keyValuePairs.end())
+        return false;
+    else
+        return true;
+}
+
+string KeyValueStore::getValue(const string key) const {
+    std::map<string, string>::const_iterator it = this->keyValuePairs.find(key);
+    if (it == keyValuePairs.end())
+        return "";
+    else
+        return it->second;
+}
+
+string KeyValueStore::getUniqueName() {
+    // These two are static within the function
+    static string prefix = "unique";
+    static unsigned int counter = 0;
+    counter++;
+    string nextname = prefix;
+    while (true) {
+        nextname = prefix + to_string(counter);
+        if (KeyValueStore::checkIfThisKeyExists(nextname) == false) {
+            break;
+        }
+    }
+    return nextname;
+}
+
+
---
+++ KeyValueStore.h
@@ -0,0 +1,53 @@
+#ifndef KEYVALUESTORE_H
+#define KEYVALUESTORE_H
+
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+
+class KeyValueStore {
+
+private:
+    map<string, string> keyValuePairs;
+    KeyValueStore();
+    static KeyValueStore *keyvalue_store;
+public:
+    bool print();
+
+    static KeyValueStore *getInstance() {
+        if (keyvalue_store == nullptr) {
+            keyvalue_store = new KeyValueStore();
+        }
+        return keyvalue_store;
+    } 
+
+    int getObjectsCount() const;
+
+    bool emptyKeyValueStore();
+
+    bool checkIfThisKeyExists(const string key);
+
+    string getValue(const string key) const;
+
+
+    // Utility functions to convert common types to string
+    
+    string to_string (int val) const { return std::to_string(val); }
+    string to_string (long val) const { return std::to_string(val); }
+    string to_string (long long val) const { return std::to_string(val); }
+    string to_string (unsigned val) const { return std::to_string(val); }
+    string to_string (unsigned long val) const { return std::to_string(val); }
+    string to_string (unsigned long long val) const { return std::to_string(val); }
+    string to_string (float val) const { return std::to_string(val); }
+    string to_string (double val) const { return std::to_string(val); }
+    string to_string (long double val) const { return std::to_string(val); }
+
+
+    string getUniqueName() ;
+
+};
+
+#endif
