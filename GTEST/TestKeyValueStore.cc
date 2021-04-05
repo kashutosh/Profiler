df975f43c3afc23ef3ab7b86f3201d1aea518b87
Panther <pantherhelfie@gmail.com>
2021-04-05 14:41:43 +0530

    Added testcase for KeyValueStore
     Changes to be committed:
    	modified:   GTEST/CMakeLists.txt
    	new file:   GTEST/TestKeyValueStore.cc
    	modified:   KeyValueStore.cc
    	modified:   KeyValueStore.h

---
+++ GTEST/TestKeyValueStore.cc
@@ -0,0 +1,30 @@
+#include <gtest/gtest.h>
+#include "../KeyValueStore.h"
+#include <iostream>
+using namespace std;
+using namespace FlightRecorder;
+
+
+TEST(FirstTestSuite, CheckingBasics) {
+
+  KeyValueStore *kvs = KeyValueStore::getInstance();
+  int numkeys = kvs->getObjectsCount();
+  ASSERT_EQ(numkeys, 0);
+
+  string nextname = kvs->getUniqueName();
+  ASSERT_EQ(nextname, "unique1");
+  nextname = kvs->getUniqueName();
+  ASSERT_EQ(nextname, "unique2");
+  cout << nextname << endl;
+
+}
+
+
+TEST(FirstTestSuite, CheckingDuplicates) {
+  KeyValueStore *kvs = KeyValueStore::getInstance();
+  bool result = kvs->insertKeyValue("key1", "value1");
+  ASSERT_EQ(result, true);
+  result = kvs->insertKeyValue("key1", "value1");
+  ASSERT_EQ(result, false);
+}
+
